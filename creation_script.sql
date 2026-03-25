49aeb1da40eb926852e99432b58f3c7285cf6d95
tatirib08 <tatiroliveira08@gmail.com>
2026-03-25 01:48:31 -0300

    script create db

---
+++ creation_script.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION "uuid-ossp";
+CREATE TABLE IF NOT EXISTS pesquisadores (
+pesquisadores_id UUID NOT NULL DEFAULT uuid_generate_v4(),
+lattes_id VARCHAR(16) NOT NULL,
+nome VARCHAR(200) NOT NULL,
+PRIMARY KEY (pesquisadores_id ));
+CREATE TABLE IF NOT EXISTS producoes (
+producoes_id UUID NOT NULL DEFAULT uuid_generate_v4(),
+pesquisadores_id UUID NOT NULL,
+issn VARCHAR(16) NOT NULL,
+nomeArtigo TEXT NOT NULL,
+anoArtigo INTEGER NOT NULL,
+PRIMARY KEY (producoes_id),
+CONSTRAINT fkey FOREIGN KEY ( pesquisadores_id)
+REFERENCES pesquisadores (pesquisadores_id) ON UPDATE NO ACTION ON DELETE NO ACTION);
